d9e898df9e256656a75a958a9737cfc782086ee1
James Wiegand <jawiegand@gmail.com>
2012-12-29 14:52:32 -0700

    Added the RHNetworking class. This class will use a singleton to manage network traffic. I also started using oxygen I suggest that we all do the same.

---
+++ src/RemoteHome/RemoteHome/RHNetworkEngine.h
@@ -0,0 +1,13 @@
+//
+//  RHNetworkEngine.h
+//  RemoteHome
+//
+//  Created by James Wiegand on 12/29/12.
+//  Copyright (c) 2012 James Wiegand. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RHNetworkEngine : NSObject
+
+@end
---
+++ src/RemoteHome/RemoteHome/RHNetworkEngine.m
@@ -0,0 +1,13 @@
+//
+//  RHNetworkEngine.m
+//  RemoteHome
+//
+//  Created by James Wiegand on 12/29/12.
+//  Copyright (c) 2012 James Wiegand. All rights reserved.
+//
+
+#import "RHNetworkEngine.h"
+
+@implementation RHNetworkEngine
+
+@end
