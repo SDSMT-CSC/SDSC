0dddd54f683419de8c53fd2018b11e60a87308c7
Nilam <christopher.jensen@mines.sdsmt.edu>
2013-03-30 15:54:12 -0600

    Turned the devices.php code inside-out and made spaghetti code into lasagna. Much tastier and easier to debug.

--- src/gatewayServer/html/devices.php
+++ src/gatewayServer/html/devices.php
@@ -1,30 +1,80 @@
 <?php
-  $topmatter = file_get_contents('header.html');
-  $title = $topmatter . "  <title>Device Menu - Remote Home Base Station</title>\n </head>\n <body>\n";
-  $users = parse_ini_file('devices.ini', true, INI_SCANNER_RAW);
-  if ($users === False)
+{
+  $prop_array = array('Name','Group','Interface');
+  include_once('util.php');
+  if(isset($_REQUEST['action']))
   {
-    $content = "<p class='error'>Something went wrong, and we could not open the devices.ini file for you. A new, empty devices file will be made for you, so feel free to add any devices you want. If you have already added devices to this system, please contact customer service.</p>\n";
+    switch($_REQUEST['action'])
+    {
+      case 'Confirm Delete Device':
+        $val = remove_node('devices.php', 'devices.ini');
+        if($val)
+        {
+          print $val;
+        }
+        else
+        {
+          print get_header('Devices Menu') . print_generic_menu('Devices', 'devices.php', 'devices.ini', array('Edit Device', 'Delete Device'), array('Add Device',) ) . get_content_tail();
+        }
+        break;
+      case 'Delete Device':
+        print confirm_remove_node('devices.php');
+        break;
+      /* Edit Device form */
+      case 'Edit Device':
+        if(isset($_REQUEST['name']) && format_from_ini($_REQUEST['name'], 'devices.ini'))
+        {
+          {
+            $str = get_header('Edit Device') . print_generic_form('Edit', 'devices.php', 'Device', $prop_array) . get_content_tail();
+            print $str;
+          }
+        }
+        else
+        {
+            print get_header('Devices Menu') . print_generic_menu('Devices', 'devices.php', 'devices.ini', array('Edit Device', 'Delete Device'), array('Add Device',) ) . get_content_tail();
+        }
+        break;
+      case 'Edit':
+        $val = validate_generic_form('Edit', 'devices.php', 'Device', $prop_array, 'devices.ini');
+        if($val != "")
+        {
+          print($val);
+          break;
+        }
+        else
+        {
+          print get_header('Device Menu') . print_generic_menu('Devices', 'devices.php', 'devices.ini', array('Edit Device', 'Delete Device'), array('Add Device',) ) . get_content_tail();
+          break;
+        }
+        break;
+      /* Add Device form */
+      case 'Add Device':
+        print get_header('Add Device') . print_generic_form('Add', 'devices.php', 'Device', $prop_array) . get_content_tail();
+        break;
+      /* Try to add a device submitted by Add Device */
+      case 'Add':
+        $val = validate_generic_form('Add', 'devices.php', 'Device', $prop_array, 'devices.ini');
+        if($val != "")
+        {
+          print($val);
+          break;
+        }
+        else
+        {
+          print get_header('Device Menu') . print_generic_menu('Devices', 'devices.php', 'devices.ini', array('Edit Device', 'Delete Device'), array('Add Device',) ) . get_content_tail();
+          break;
+        }
+      /* If everything else didn't match, print a menu. */
+      default: 
+        print $_REQUEST['action'];
+        print get_header('Devices Menu') . print_generic_menu('Devices', 'devices.php', 'devices.ini', array('Edit Device', 'Delete Device'), array('Add Device',) );
+        break;
+    }
   }
   else
   {
-    $content = "<h1>Devices</h1>\n";
+    $str = get_header('Devices Menu') . print_generic_menu('Devices', 'devices.php', 'devices.ini', array('Edit Device', 'Delete Device'), array('Add Device',) ) . get_content_tail();
+    print $str;
   }
-  $content .= "<form action='device_update.php' method='POST'>\n";
-  $content .= " <select name='name'>\n";
-  foreach ($users as $user => $attrlist)
-  {
-    $content .= "  <option>$user</option>\n";
-  }
-  $content .= " </select>\n";
-  $content .= " <input type='submit' name='action' value='Edit Device' />\n";
-  $content .= " <input type='submit' name='action' value='Delete Device'/>\n";
-  $content .= "</form>\n";
-  $content .= "<form action='device_update.php' method='POST'>\n";
-  $content .= " <input type='submit' name='action' value='Add Device'/>\n";
-  $content .= "</form>\n";
-  $content .= "</body>\n";
-  $content .= "</html>\n";
-  print $title;
-  print $content;
+}
 ?>
