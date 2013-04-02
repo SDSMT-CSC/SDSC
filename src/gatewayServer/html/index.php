903d03c6e1ad6fb624a0728e3700259bdf79aeb0
Nilam <christopher.jensen@mines.sdsmt.edu>
2013-04-02 08:49:08 -0600

    Added some style to the php for the base station front end.

---
+++ src/gatewayServer/html/index.php
@@ -0,0 +1,4 @@
+<?php
+include('util.php');
+    print get_header('') . get_content_head() . "<p>Please select a link at the left.</p>\n" . get_content_tail();
+?>
--- src/gatewayServer/html/users.php
+++ src/gatewayServer/html/users.php
@@ -1,4 +1,83 @@
 <?php
+{
+  $prop_array = array('Name','Group');
+  include_once('util.php');
+  if(isset($_REQUEST['action']))
+  {
+    switch($_REQUEST['action'])
+    {
+      case 'Confirm Delete User':
+        $val = remove_node('users.php', 'users.ini');
+        if($val)
+        {
+          print $val;
+        }
+        else
+        {
+          print get_header('Users Menu') . print_generic_menu('Users', 'users.php', 'users.ini', array('Edit User', 'Delete User'), array('Add User',) ) . get_content_tail();
+        }
+        break;
+      case 'Delete User':
+        print confirm_remove_node('users.php');
+        break;
+      /* Edit User form */
+      case 'Edit User':
+        if(isset($_REQUEST['name']) && format_from_ini($_REQUEST['name'], 'users.ini'))
+        {
+          {
+            $str = get_header('Edit User') . print_generic_form('Edit', 'users.php', 'User', $prop_array) . get_content_tail();
+            print $str;
+          }
+        }
+        else
+        {
+            print get_header('Users Menu') . print_generic_menu('Users', 'users.php', 'users.ini', array('Edit User', 'Delete User'), array('Add User',) ) . get_content_tail();
+        }
+        break;
+      case 'Edit':
+        $val = validate_generic_form('Edit', 'users.php', 'User', $prop_array, 'users.ini');
+        if($val != "")
+        {
+          print($val);
+          break;
+        }
+        else
+        {
+          print get_header('User Menu') . print_generic_menu('Users', 'users.php', 'users.ini', array('Edit User', 'Delete User'), array('Add User',) ) . get_content_tail();
+          break;
+        }
+        break;
+      /* Add User form */
+      case 'Add User':
+        print get_header('Add User') . print_generic_form('Add', 'users.php', 'User', $prop_array) . get_content_tail();
+        break;
+      /* Try to add a user submitted by Add User */
+      case 'Add':
+        $val = validate_generic_form('Add', 'users.php', 'User', $prop_array, 'users.ini');
+        if($val != "")
+        {
+          print($val);
+          break;
+        }
+        else
+        {
+          print get_header('User Menu') . print_generic_menu('Users', 'users.php', 'users.ini', array('Edit User', 'Delete User'), array('Add User',) ) . get_content_tail();
+          break;
+        }
+      /* If everything else didn't match, print a menu. */
+      default: 
+        print $_REQUEST['action'];
+        print get_header('Users Menu') . print_generic_menu('Users', 'users.php', 'users.ini', array('Edit User', 'Delete User'), array('Add User',) );
+        break;
+    }
+  }
+  else
+  {
+    $str = get_header('Users Menu') . print_generic_menu('Users', 'users.php', 'users.ini', array('Edit User', 'Delete User'), array('Add User',) ) . get_content_tail();
+    print $str;
+  }
+}
+/*
   $topmatter = file_get_contents('header.html');
   $title = $topmatter . "  <title>User Menu - Remote Home Base Station</title>\n </head>\n <body>\n";
   $users = parse_ini_file('users.ini', true, INI_SCANNER_RAW);
@@ -31,4 +110,5 @@
   $content .= "</html>\n";
   print $title;
   print $content;
+*/
 ?>
